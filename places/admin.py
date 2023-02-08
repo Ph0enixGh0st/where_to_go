78069c3e9fdde015c64c55675bf929b9bdf542e8
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2023-02-09 01:48:09 +0300

    Added model for images
    Added model for images

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Venue
+from .models import Venue, VenuePhoto
 
 
 @admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
     list_display = ('title', )
+
+
+@admin.register(VenuePhoto)
+class VenuePhotoAdmin(admin.ModelAdmin):
+    list_display = ('show_order_venue', )
--- places/models.py
+++ places/models.py
@@ -2,10 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+
 class Venue(models.Model):
     title = models.TextField()
-    imgs = models.ImageField(upload_to='venues_pics', blank=True)
     description_short = models.TextField()
     description_long = models.TextField()
     lng = models.DecimalField(max_digits=17, decimal_places=14)
     lat = models.DecimalField(max_digits=17, decimal_places=14)
+
+    def __str__(self):
+        return self.title
+
+
+class VenuePhoto(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='venue_pics', blank=True, null=True)
+    order = models.SmallIntegerField('Order #')
+
+    class Meta:
+        unique_together = ('venue', 'order', )
+
+    @property
+    def show_order_venue(self):
+        return f'{self.order}: {self.venue}'
+
+    def __str__(self):
+        return f'Photo #{self.order} from {self.venue}'
