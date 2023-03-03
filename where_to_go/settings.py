45cc48ef727c15cee29f972f413de32a15a9b4b9
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2023-03-04 00:21:10 +0300

    env update
    env update

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = env("STATIC_URL")
 STATICFILES_DIRS = [STATIC_DIR, ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, env("MEDIA_ROOT"))
+MEDIA_URL = env("MEDIA_URL")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
