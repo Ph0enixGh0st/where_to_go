090d1267aa527e8b3f58bcd481dfe851ec42bf92
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2023-05-26 18:57:23 +0300

    Changed DEBUG variable in settings.py to environs standard with default value
    DEBUG = env.str('DEBUG') ->
    DEBUG = env.bool('DEBUG', False)

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -28,7 +28,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.str('DEBUG')
+
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', '127.0.0.1')
 
